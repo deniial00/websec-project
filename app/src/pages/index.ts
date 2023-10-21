9ce83147e96ac51430bd30deb2d0bd624d6989e5
if21b045 <if21b045@technikum-wien.at>
2023-10-21 20:16:58 +0200

    modified index.ts and navbar-component.ts + minor other changes
    in index.ts sind jetzt die anderen komponente auch imported. die selected pages und available_pages sind jetzt soweit auch fertig. falls parameter zu den components geaddet werden sollen kann das jetzt gleich in den available_pages gemacht werden.

--- app/src/pages/index.ts
+++ app/src/pages/index.ts
@@ -1,20 +1,29 @@
-import { LitElement, css, html } from 'lit'
+import { LitElement, TemplateResult, css, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
+import {literal} from 'lit/static-html.js';
 
 import '../components/navbar-component'
 import './tickets-page'
+import './ticket-page'
+import './profile-page'
+import './login-page'
 
 @customElement('app-page')
 export class App extends LitElement {
 
 	@property({ type: Boolean })
-	is_logged_in = false;
+	is_logged_in = true;
 
 	@property({type: Array})
-	available_pages = ["login-page", "tickets-page", "ticket-page", "profile-page"];
+	available_pages: Record<string, TemplateResult> = {
+		"login-page": html `<login-page></login-page>`,
+		"tickets-page": html `<tickets-page></tickets-page>`,
+		"ticket-page": html `<ticket-page></ticket-page>`,
+		"profile-page": html `<profile-page></profile-page>`,
+	};
 
-	@property({type: String})
-	selected_page = "tickets-page";
+	@property({ type: String })
+	selected_page = this.available_pages["tickets-page"];
 
 	static styles = css`
 		:host {
@@ -34,16 +43,23 @@ export class App extends LitElement {
 		this.requestUpdate();
 	}
 
+	private onPageChanged(event: { detail: { new_page: string } }) {
+		this.selected_page = this.available_pages[event.detail.new_page];
+		this.requestUpdate();
+	}
+
+
 	render() {
 		return html`
 			<header>
-				<navbar-component .is_logged_in="${this.is_logged_in}" @login-status-changed="${this.onLoginStatusChanged}"></navbar-component>
+				<navbar-component .is_logged_in="${this.is_logged_in}" @login-status-changed="${this.onLoginStatusChanged}" @page-changed="${this.onPageChanged}"></navbar-component>
 			</header>
 			<div class="main-content">
-				<tickets-page></tickets-page>
-				<ticket-page></ticket-page>
-				<profile-page></profile-page>
-			<div>
+			${this.is_logged_in
+				? html `${this.selected_page}`
+				: html`<login-page></login-page>`
+			  }
+			</div>
 		`
 	}
 }
