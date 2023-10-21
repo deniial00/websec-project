2bdfbae6c1f24d664a09bcfbc30a9e82ce57cd09
Daniel Hofbauer <hofbauer.d@icloud.com>
2023-10-21 17:44:48 +0200

    handleChangePage added WIP

--- app/src/components/navbar-component.ts
+++ app/src/components/navbar-component.ts
@@ -5,18 +5,21 @@ import { customElement, property } from 'lit/decorators.js'
 @customElement('navbar-component')
 export class NavbarComponent extends LitElement {
 
-	@property({type: String})
-	docsHint = ''
-
 	render() {
 		return html`
 			<div>
-				<div>Login</div>
-				<div>Tickets</div>
-				<div>Profile</div>
-				<div>Logout</div>
+				<div @click="${this._handleChangePage}">Login</div>
+				<div @click="${this._handleChangePage}">Tickets</div>
+				<div @click="${this._handleChangePage}">Profile</div>
+				<div @click="${this._handleChangePage}">Logout</div>
 			</div>
 		`
+	}
+
+	private _handleChangePage(e: Event) {
+		// funkt nd - aber so hätt ichs ma vorgestellt. dann in app auf das event listenen
+		const pageEvent = new CustomEvent('changed-page',{ {currentPage: 'test'}, composed: true, cancelable: true });
+		this.dispatchEvent(pageEvent);
 	}
 
 	static styles = css`
