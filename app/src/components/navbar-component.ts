b026aa4c1462bddc27a0c0d2cc8508b1f61dfa75
if21b045 <if21b045@technikum-wien.at>
2023-10-21 18:10:45 +0200

    modified index.ts and navbar-component.ts
    bei der index seite hab ich jezt das ganze mal so umgeändert, dass man available pages hat und eine selected page; das event onLoginStatusChanged hab ich auch erstellt und das funktioniert soweit.
    
    beim navbar component hab ich jetzt einmal ein bisschen styling hinzugefügt. ich hab außerdem die login option entfernt weils meiner meinung nach keinen sinn macht, die anzuzeigen und die logout option zeig ich nur an, wenn man eingeloggt ist.
    
    nächste schritte: navbar umbauen, sodass die navigation tatsächlich funktioniert und in der indexseite dementsprechend auch nur die aktive seite anzeigen

--- app/src/components/navbar-component.ts
+++ app/src/components/navbar-component.ts
@@ -5,26 +5,49 @@ import { customElement, property } from 'lit/decorators.js'
 @customElement('navbar-component')
 export class NavbarComponent extends LitElement {
 
-	render() {
-		return html`
-			<div>
-				<div @click="${this._handleChangePage}">Login</div>
-				<div @click="${this._handleChangePage}">Tickets</div>
-				<div @click="${this._handleChangePage}">Profile</div>
-				<div @click="${this._handleChangePage}">Logout</div>
-			</div>
-		`
+	@property({ type: Boolean })
+	is_logged_in: boolean | undefined;
+
+	private _change_login_status = () => {
+		this.dispatchEvent(new CustomEvent('login-status-changed', {
+			detail: { is_logged_in: !this.is_logged_in },
+			bubbles: true,
+			composed: true,
+			}));
 	}
 
 	private _handleChangePage(e: Event) {
 		// funkt nd - aber so hätt ichs ma vorgestellt. dann in app auf das event listenen
-		const pageEvent = new CustomEvent('changed-page',{ {currentPage: 'test'}, composed: true, cancelable: true });
-		this.dispatchEvent(pageEvent);
+		//const pageEvent = new CustomEvent('changed-page',{ {currentPage: 'test'}, composed: true, cancelable: true });
+		//this.dispatchEvent(pageEvent);
 	}
 
 	static styles = css`
- 
+		:host {
+			display: flex;	/* horizontal alignment */
+			justify-content: flex-end;	/* horizontal alignment */
+			height: 3.5em;
+			background-color: rgb(36, 36, 36);
+			color: white;
+		}
+
+		:host div {
+			display: flex;	/* display items next to each other */
+			align-items: center;	/* vertical alignment of divs in parent */
+			margin-left: 0.2em;
+			margin-right: 0.2em;
+		}
 	`;
+
+	render() {
+		return html`
+			<div @click="${this._handleChangePage}">Tickets</div>
+			<div @click="${this._handleChangePage}">Profile</div>
+			${this.is_logged_in ? html`
+				<div @click="${this._handleChangePage}">Logout</div>
+			`: html``}
+			`
+	}
 }
 
 declare global {
--- app/src/pages/index.ts
+++ app/src/pages/index.ts
@@ -7,21 +7,45 @@ import './tickets-page'
 @customElement('app-page')
 export class App extends LitElement {
 
+	@property({ type: Boolean })
+	is_logged_in = false;
+
+	@property({type: Array})
+	available_pages = ["login-page", "tickets-page", "ticket-page", "profile-page"];
+
 	@property({type: String})
-	activePage = 'Tickets';
+	selected_page = "tickets-page";
+
+	static styles = css`
+		:host {
+			min-height: 100vh;
+			margin: 0;
+			display: flex;
+			flex-direction: column;
+		}
+
+		.main_content {
+			flex-grow: 1;
+		}
+	`
+
+	private onLoginStatusChanged(event: { detail: { is_logged_in: boolean } }) {
+		this.is_logged_in = event.detail.is_logged_in;
+		this.requestUpdate();
+	}
 
 	render() {
 		return html`
-			<navbar-component></navbar-component>
-			<tickets-page></tickets-page>
-			<ticket-page></ticket-page>
-			<profile-page></profile-page>
+			<header>
+				<navbar-component .is_logged_in="${this.is_logged_in}" @login-status-changed="${this.onLoginStatusChanged}"></navbar-component>
+			</header>
+			<div class="main-content">
+				<tickets-page></tickets-page>
+				<ticket-page></ticket-page>
+				<profile-page></profile-page>
+			<div>
 		`
 	}
-
-	static styles = css`
- 
-	`
 }
 
 declare global {
