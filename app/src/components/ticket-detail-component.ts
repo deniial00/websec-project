33ac955a40732769eca787f4b52f52a236c0105d
Daniel Hofbauer <hofbauer.d@icloud.com>
2023-10-23 02:05:05 +0200

    funktioniert mit fetch von db

--- app/src/components/ticket-detail-component.ts
+++ app/src/components/ticket-detail-component.ts
@@ -2,24 +2,29 @@ import { LitElement, css, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 
 import { Ticket } from '../interfaces/ticket-interface'
+import { when } from 'lit/directives/when.js';
 
 @customElement('ticket-detail')
 export class TicketDetail extends LitElement {
 
 	// ! to turn off error for undefined ticket var
 	@property({type: Object})
-	ticket!: Ticket;
+	ticket: Ticket | undefined;
 
 	constructor() {
 		super();
 	}
 
 	render() {
-		return html`
-			<div>${this.ticket.title}</div>
-			<div>${this.ticket.author.name} am ${this.ticket.creationDate.toLocaleDateString('de-DE')}</div>
-			<div>${this.ticket.content}</div>
-		`
+		return when(this.ticket,
+			() => html`
+					<div>${this.ticket!.title}</div>
+					<div>${this.ticket!.author.name} am ${new Date(this.ticket!.creationDate).toLocaleDateString('de-DE')}</div>
+					<div>${this.ticket!.content}</div>
+			`,
+			() => html`<div>Wähle ein Ticket aus</div>`
+		)
+		
 	}
 
 	static styles = css`
--- app/src/pages/tickets-page.ts
+++ app/src/pages/tickets-page.ts
@@ -1,6 +1,5 @@
 import { LitElement, css, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
-import { Task } from '@lit/Task'
 
 import { Ticket } from '../interfaces/ticket-interface'
 
@@ -11,97 +10,38 @@ import '../components/ticket-list-component';
 export class TicketsPage extends LitElement {
 
 	@property({type: Array})
-	tickets: Ticket[] = [
-		{
-			author: {
-				name: 'Daniel Hofbauer',
-				uid: '9363fa7d-1f95-4098-ae62-5981906f2f10'
-			},
-			creationDate: new Date(),
-			title: 'Test ticket',
-			content: 'Reprehenderit aliquip nostrud et velit labore labore. Cupidatat nulla nisi ipsum adipisicing labore anim aliqua aute eiusmod cillum ut aute. Do elit deserunt fugiat officia aliquip sunt esse aute ut sint. Consequat velit ipsum deserunt ea eiusmod tempor laborum ut.',
-			uuid: '550e8400-e29b-41d4-a716-446655440000',
-			status: 'open'
-		},
-		{
-			author: {
-				name: 'Daniel Hofbauer',
-				uid: '9363fa7d-1f95-4098-ae62-5981906f2f10'
-			},
-			creationDate: new Date(),
-			title: 'Test ticket2',
-			content: 'Laborum amet fugiat qui quis non duis sint. Ad id occaecat incididunt reprehenderit sint cupidatat fugiat in officia cupidatat deserunt cillum officia ipsum. Eu officia do deserunt voluptate eiusmod duis tempor laborum ullamco qui reprehenderit. Proident elit duis nostrud ullamco velit. Quis enim laborum commodo ipsum. Eiusmod duis reprehenderit ut mollit reprehenderit.',
-			uuid: '7c7f8ef8-6a10-4f7c-a12e-84a7fffc2f94',
-			status: 'open'
-		},
-		{
-			author: {
-				name: 'Daniel Hofbauer',
-				uid: '9363fa7d-1f95-4098-ae62-5981906f2f10'
-			},
-			creationDate: new Date(),
-			title: 'Neues Test Ticket',
-			content: 'Laborum amet fugiat qui quis non duis sint. Ad id occaecat incididunt reprehenderit sint cupidatat fugiat in officia cupidatat deserunt cillum officia ipsum. Eu officia do deserunt voluptate eiusmod duis tempor laborum ullamco qui reprehenderit. Proident elit duis nostrud ullamco velit. Quis enim laborum commodo ipsum. Eiusmod duis reprehenderit ut mollit reprehenderit.',
-			uuid: '1dd2b63e-f46e-47a9-9e3f-ce61719059ad',
-			status: 'open'
-		},
-		{
-			author: {
-				name: 'Daniel Hofbauer',
-				uid: '9363fa7d-1f95-4098-ae62-5981906f2f10'
-			},
-			creationDate: new Date(),
-			title: 'Das ist kein Test mehr.',
-			content: 'Laborum amet fugiat qui quis non duis sint. Ad id occaecat incididunt reprehenderit sint cupidatat fugiat in officia cupidatat deserunt cillum officia ipsum. Eu officia do deserunt voluptate eiusmod duis tempor laborum ullamco qui reprehenderit. Proident elit duis nostrud ullamco velit. Quis enim laborum commodo ipsum. Eiusmod duis reprehenderit ut mollit reprehenderit.',
-			uuid: '0657865c-03aa-4954-ab65-b70b5f1fa77a',
-			status: 'open'
-		},
-		{
-			author: {
-				name: 'Mario Gussnig',
-				uid: '906d1a9b-1e15-4dd3-9591-c02f8a73890a'
-			},
-			creationDate: new Date(),
-			title: 'Das ist doch wieder ein Test',
-			content: 'Laborum amet fugiat qui quis non duis sint. Ad id occaecat incididunt reprehenderit sint cupidatat fugiat in officia cupidatat deserunt cillum officia ipsum. Eu officia do deserunt voluptate eiusmod duis tempor laborum ullamco qui reprehenderit. Proident elit duis nostrud ullamco velit. Quis enim laborum commodo ipsum. Eiusmod duis reprehenderit ut mollit reprehenderit.',
-			uuid: '0657865c-03aa-4954-ab65-b70b5f1fa77a',
-			status: 'open'
-		}
-	]
+	tickets: Ticket[] = [];
 
-	@property({type: String})
-	activeTicket = ''
+	@property({type: Object})
+	activeTicket: Ticket | undefined;
+
+	userId: string;
 
 	constructor() {
 		super()
-		this.loadTickets.run();
-		this.activeTicket = this.tickets[0].uuid;
+		this.fetchTickets();
 		this.userId = '1';
 	}
 
-	// mega praktisch: wenn man das als get definiert, dann wird das beim binding nicht als function angesehen und führt dann zu keinem type mismatch (() => obj != obj)
-	get getActiveTicket() : Ticket {
-		const ticket = this.tickets.find( ticket => ticket.uuid === this.activeTicket)
-		
-		if (ticket == undefined)
-			return
-		
-		return ticket
+	private async fetchTickets() {
+		const response = await fetch('http://localhost/api/tickets');
+		const json = await response.json();
+
+		if (!json || json === undefined)
+			throw Error(`Ticket nicht gefunden`);
+
+		try {
+			this.tickets = await response.json() as Ticket[];
+		} catch(e) {
+			
+		}
 	}
 
 	handleActiveTicketChanged(e: { detail: { activeTicket: string} }) {
-		this.activeTicket = e.detail.activeTicket;
+		const uuid = e.detail.activeTicket;
+		this.activeTicket = this.tickets.find( ticket => ticket.uuid === uuid);
+		console.log(this.activeTicket);
 	}
-
-	private loadTickets = new Task(this, {
-		task: async ([], {signal}) => {
-			const response = await fetch(`http://localhost/api?collection=users&operation=get`, {signal});
-			if (!response.ok) { throw new Error(response.status); }
-			// return response.json() as Ticket;
-			this.tickets = response;
-		},
-		args: () => [this.userId]
-	});
 
 	render() {
 		return html`
@@ -109,7 +49,7 @@ export class TicketsPage extends LitElement {
 				.tickets='${this.tickets}'
 				@active-ticket-changed="${this.handleActiveTicketChanged}"
 			></ticket-list>
-			<ticket-detail .ticket='${this.getActiveTicket}'></ticket-detail>
+			<ticket-detail .ticket='${this.activeTicket}'></ticket-detail>
 		`
 	}
 
